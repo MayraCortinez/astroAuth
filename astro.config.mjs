f7595db9dd9f9dcf051afa09ec02b676df456ce1
MayC <mayrautopia@gmail.com>
2024-05-20 10:28:19 -0300

    without linkedin add tailwind class and icons svg

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import auth from "auth-astro";
-
 import vercel from "@astrojs/vercel/serverless";
+import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [auth()],
+  integrations: [auth(), tailwind(), react()],
   output: "server",
   adapter: vercel()
 });
